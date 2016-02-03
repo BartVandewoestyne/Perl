5f53f96498a82e4364a9d56dd0f056c3f7100e2d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-02-03 12:16:12 +0100

    Add hello world example.

---
+++ examples/hello_world.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+#
+# The traditional first program.
+#
+# References:
+#
+#   [1] http://search.cpan.org/dist/Perl-Tutorial/lib/Perl/Tutorial/HelloWorld.pod
+
+# Strict and warnings are recommended.
+use strict;
+use warnings;
+
+# Print a message.
+print "Hello, World!\n";
