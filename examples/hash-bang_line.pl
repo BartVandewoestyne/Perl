f329d08863a72c88ad77104dd13090e9017d6822
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-14 10:06:31 +0100

    Add example on hash-bang line.

---
+++ examples/hash-bang_line.pl
@@ -0,0 +1,12 @@
+# References:
+#
+#   [perlmaven] https://perlmaven.com/hashbang
+
+# Quite similar to what use warnings does, but old-style.
+#!/usr/bin/perl -w
+
+# New style
+#!/usr/bin/perl
+
+use strict;
+use warnings;
