528f8ea9ef6b23107398c1420ea81785977934ee
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-14 10:08:32 +0100

    Add example on say.

---
+++ examples/say.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use 5.010;
+use strict;
+use warnings;
+
+# say is like print but it is shorter and it automatically adds a newline at
+# the end.
+print "Hello World\n";
+say "Hello World";
